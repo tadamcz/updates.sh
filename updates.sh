d230b7a855db857b861ecf94e358e4c87f736991
tadamcz <tadamczewskipublic@gmail.com>
2023-06-07 00:34:59 +0100

    homebrew download parallelism
    homebrew refuses to be smart about when it downloads packages, so this makes a big difference if there are many casks/formulae to upgrade

--- updates.sh
+++ updates.sh
@@ -2,30 +2,53 @@
 
 sudo -v  # ask for password once up front (or cache it)
 
+# 1. Homebrew
+# List Homebrew formulae
+echo "Listing outdated formulae..."
+formulae=($(brew outdated  --quiet --formula))
 
-# 1. Formulae
-echo "Upgrading formulae..."
-brew upgrade --formula --ignore-pinned
-
-# 2. Casks
+# List Homebrew casks
 # Note: It's recommended to set `HOMEBREW_CASK_OPTS=--no-quarantine`, or you will get lots of annoying MacOS prompts
 # next time you open a cask after updating (and casks will silently fail to start on login!)
-sudo -v  # cache sudo
 casks_no_upgrade="alfred"  # Can use regex "or" to list multiple, e.g. "alfred|google-cloud-sdk"
-echo "Upgrading casks (except ${casks_no_upgrade})..."
+echo "Listing outdated casks (except ${casks_no_upgrade})..."
+casks=($(brew outdated --quiet --greedy-auto-updates --cask | grep --invert-match -E $casks_no_upgrade))
 
-list=$(brew outdated --greedy-auto-updates --cask --quiet | grep --invert-match -E $casks_no_upgrade)
+# Prime the cache (using `brew fetch`) in parallel (using `pueue`)
+pueued --daemonize
+parallelism=12
+pueue group add brew_fetch
+pueue parallel $parallelism --group brew_fetch
+for formula in ${formulae[@]}; do
+    command="brew fetch --formula $formula"
+    pueue add --group brew_fetch $command >& /dev/null
+done
+for cask in ${casks[@]}; do
+    command="brew fetch --cask $cask"
+    pueue add --group brew_fetch $command >& /dev/null
+done
 
-if [ -n "${list}" ]
-then
-	echo $list | xargs brew upgrade --greedy-auto-updates --cask --quiet
-else
-	echo "No casks to upgrade"
-fi
+echo "Downloading ${#formulae[@]} formulae and ${#casks[@]} casks with parallelism $parallelism. This may take a while..."
+pueue wait --group brew_fetch
+pueue log
+pueue clean --group brew_fetch
+pueue group remove brew_fetch
+pueue shutdown
 
-# 3. Poetry
+# Upgrade formulae and casks
+sudo -v # re-cache sudo credentials, or ask for password
+echo "Upgrading formulae..."
+brew upgrade --formula --ignore-pinned $formulae
+echo "Upgrading casks..."
+brew upgrade --cask --greedy-auto-updates $casks
+
+
+# 2. Poetry
 poetry self update
 
-# 4. asdf version manager plugins
+# 3. asdf version manager plugins
 echo "Updating asdf plugins..."
 asdf plugin-update --all
+
+# 4. Mac App Store
+mas upgrade
